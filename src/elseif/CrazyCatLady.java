d4a7372f8308649b96d6015128fcaf5de30a5e38
tippur.sanchitha <tippur.sanchitha@SEMS-259-15.SEMS259.LAB>
2018-02-28 14:52:40 -0800

    Cats

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,17 +5,28 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String response=JOptionPane.showInputDialog("How many cats do you have?");
 		// 2. Convert their answer into an int
-
+int num0=Integer.parseInt(response);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+if(num0>3) {
+Syso ("YOU CRAZY CAT HORDER!!!!!!!!!");
+}
 
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
+		
+	
+
+
+	private static void JOptionPane;(String string) {
+		// TODO Auto-generated method stub
 		
 	}
 
@@ -30,3 +41,4 @@ public class CrazyCatLady {
 
 }
 
+}
